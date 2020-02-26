4ce2c50f057f44c226b007e0d25190bb83b81ec4
Hirokazu Hata <h.hata.ai.t@gmail.com>
2020-02-27 00:25:56 +0900

    signatureHelp: add textDocument/signatureHelp autocmd

---
+++ lua/nvim_lsp_contrib.lua
@@ -0,0 +1,10 @@
+local util = require'nvim_lsp_contrib/util'
+local autocmd = require'nvim_lsp_contrib/autocmd'
+local callbacks = require'nvim_lsp_contrib/callbacks'
+local M = {
+  util = util,
+  autocmd = autocmd,
+  callbacks = callbacks,
+}
+
+return M
---
+++ lua/nvim_lsp_contrib/autocmd.lua
@@ -0,0 +1,36 @@
+local vim = vim
+local api = vim.api
+local lsp = vim.lsp
+local uv = vim.loop
+local util = require'nvim_lsp_contrib/util'
+
+local M = {
+  debounce_timer = nil
+}
+
+function M.set_signature_help_autocmd(wait)
+  if wait == nil then wait = 500 end
+  api.nvim_command('augroup nvim_lsp_signature_help')
+  api.nvim_command('autocmd!')
+  api.nvim_command(string.format("autocmd CursorMoved,CursorMovedI * lua require'nvim_lsp_contrib'.autocmd.on_cursor_moved(%s)", wait))
+  api.nvim_command('augroup END')
+end
+
+function M.unset_signature_help_autocmd()
+  api.nvim_command('augroup nvim_lsp_signature_help')
+  api.nvim_command('autocmd!')
+  api.nvim_command('augroup END')
+end
+
+function M.on_cursor_moved(wait)
+  vim.validate{wait={wait,'n'}}
+  if M.debounce_timer ~= nil then
+    if uv.is_active(M.debounce_timer) then
+      util.clear_timeout(M.debounce_timer)
+    end
+    M.debounce_timer = nil
+  end
+  M.debounce_timer = util.set_timeout(wait, vim.schedule_wrap(lsp.buf.signature_help))
+end
+
+return M
